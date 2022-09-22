be5e43cd0cca50dea0bde87d6c22609ccd1c2e62
4n3ver <4n3ver@reborn.com>
2022-09-21 23:03:10 -0400

    add custom doh servers

--- scoopInstall.ps1
+++ scoopInstall.ps1
@@ -18,6 +18,7 @@ Set-EnvVar "SAM_CLI_TELEMETRY" "0"
 Set-EnvVar "AZURE_CORE_COLLECT_TELEMETRY" "0"
 Set-EnvVar "DOTNET_CLI_TELEMETRY_OPTOUT" "true"
 Set-EnvVar "RUST_BACKTRACE" "full"
+Set-EnvVar "QT_BEARER_POLL_TIMEOUT" "-1"
 
 Invoke-Expression (New-Object System.Net.WebClient).DownloadString("https://get.scoop.sh")
 
@@ -164,3 +165,37 @@ sudo New-ItemProperty `
     -Value 1 `
     -PropertyType DWORD `
     -Force
+
+sudo Add-DnsClientDohServerAddress `
+    -ServerAddress 94.140.14.14 `
+    -DohTemplate https://dns.adguard-dns.com/dns-query `
+    -AutoUpgrade 1
+sudo Add-DnsClientDohServerAddress `
+    -ServerAddress 94.140.15.15 `
+    -DohTemplate https://dns.adguard-dns.com/dns-query `
+    -AutoUpgrade 1
+sudo Add-DnsClientDohServerAddress `
+    -ServerAddress 2a10:50c0::ad1:ff `
+    -DohTemplate https://dns.adguard-dns.com/dns-query `
+    -AutoUpgrade 1
+sudo Add-DnsClientDohServerAddress `
+    -ServerAddress 2a10:50c0::ad2:ff `
+    -DohTemplate https://dns.adguard-dns.com/dns-query `
+    -AutoUpgrade 1
+
+sudo Add-DnsClientDohServerAddress `
+    -ServerAddress 1.1.1.2 `
+    -DohTemplate https://security.cloudflare-dns.com/dns-query `
+    -AutoUpgrade 1
+sudo Add-DnsClientDohServerAddress `
+    -ServerAddress 1.0.0.2 `
+    -DohTemplate https://security.cloudflare-dns.com/dns-query `
+    -AutoUpgrade 1
+sudo Add-DnsClientDohServerAddress `
+    -ServerAddress 2606:4700:4700::1112 `
+    -DohTemplate https://security.cloudflare-dns.com/dns-query `
+    -AutoUpgrade 1
+sudo Add-DnsClientDohServerAddress `
+    -ServerAddress 2606:4700:4700::1002 `
+    -DohTemplate https://security.cloudflare-dns.com/dns-query `
+    -AutoUpgrade 1
